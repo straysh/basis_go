12ed3fffda6abe33c4f75d58564262bd03996eb7
straysh <jobhancao@gmail.com>
2020-02-20 19:50:58 +0800

    数据结构-二叉树节点

--- datastructure/stack.go
+++ datastructure/stack.go
@@ -28,3 +28,32 @@ func (s *stackInt) Push(item int) {
 func (s *stackInt) Size() int {
 	return s.size
 }
+
+type stackBT struct {
+	data []*BTNode
+	size int
+}
+
+func NewStackBT(size int) *stackBT {
+	instance := &stackBT{}
+	instance.data = make([]*BTNode, size)
+
+	return instance
+}
+
+func (s *stackBT) Pop() *BTNode {
+	item := s.data[s.size-1]
+	s.data = s.data[0 : s.size-1]
+	s.size--
+
+	return item
+}
+
+func (s *stackBT) Push(item *BTNode) {
+	s.data[s.size] = item
+	s.size++
+}
+
+func (s *stackBT) Size() int {
+	return s.size
+}
--- datastructure/stack_test.go
+++ datastructure/stack_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestNormal(t *testing.T) {
+func TestStackInt(t *testing.T) {
 	stack1 := NewStackInt(10)
 	stack1.Push(1)
 	stack1.Push(2)
@@ -14,5 +14,16 @@ func TestNormal(t *testing.T) {
 
 	for stack1.Size() != 0 {
 		fmt.Println(stack1.Pop())
+	}
+}
+
+func TestStackBT(t *testing.T) {
+	stack := NewStackBT(10)
+	stack.Push(&BTNode{data: 1})
+	stack.Push(&BTNode{data: 2})
+	stack.Push(&BTNode{data: 3})
+
+	for stack.Size() != 0 {
+		fmt.Println(stack.Pop())
 	}
 }
