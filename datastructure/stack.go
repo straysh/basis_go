2d570bd2a95211ce40c86e207508bd681afa8123
straysh <jobhancao@gmail.com>
2020-02-23 14:24:20 +0800

    数据结构-简单栈

--- datastructure/stack.go
+++ datastructure/stack.go
@@ -1,5 +1,6 @@
 package datastructure
 
+// int
 type stackInt struct {
 	data []int
 	size int
@@ -29,6 +30,37 @@ func (s *stackInt) Size() int {
 	return s.size
 }
 
+// string
+type stackString struct {
+	data []string
+	size int
+}
+
+func NewStackString(size int) *stackString {
+	instance := &stackString{}
+	instance.data = make([]string, size)
+
+	return instance
+}
+
+func (s *stackString) Pop() string {
+	item := s.data[s.size-1]
+	s.data[s.size-1] = ""
+	s.size--
+
+	return item
+}
+
+func (s *stackString) Push(item string) {
+	s.data[s.size] = item
+	s.size++
+}
+
+func (s *stackString) Size() int {
+	return s.size
+}
+
+// binary tree
 type stackBT struct {
 	data []*BTNode
 	size int
